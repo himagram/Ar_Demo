6fbac739b397cbb3325042d782ff5ce4a46b2d55
HiepDD <duong.dinh.hiep@usol-v.com.vn>
2018-08-27 17:29:24 +0700

    111

--- main.js
+++ main.js
@@ -441,8 +441,8 @@
             this.cGamma = evt.gamma;
             
             i = i + 1;
-            this.el.quaternion.setFromEuler(new THREE.Euler(THREE.Math.degToRad(evt.beta), THREE.Math.degToRad(evt.alpha), -THREE.Math.degToRad(evt.gamma), 'YXZ'));
-            this.el.quaternion.multiply(new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));
+            this.el.object3D.quaternion.setFromEuler(new THREE.Euler(THREE.Math.degToRad(evt.beta), THREE.Math.degToRad(evt.alpha), -THREE.Math.degToRad(evt.gamma), 'YXZ'));
+            this.el.object3D.quaternion.multiply(new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));
             document.querySelector("#test_el").innerText = "test:" + i;
 
             //var heading = null;
