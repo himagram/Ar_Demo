2740865441275dfb1265446144dacdab30f2ed4c
HiepDD <duong.dinh.hiep@usol-v.com.vn>
2018-08-27 14:49:35 +0700

    -----

--- main.js
+++ main.js
@@ -633,7 +633,7 @@
     window.addEventListener('deviceorientation', (orientation) => {
         // TODO: 磁石の北と真北のズレを修正しなくていいのか確認する。
         var el = document.querySelector("#camera");
-        document.querySelector("#test_el2").innerText = "rotationX: " + el.getAttribute('rotation').x + ";rotationY: " + el.getAttribute('rotation').y + ";rotationZ: " + el.getAttribute('rotation').z;
+        document.querySelector("#test_el").innerText = "rotationX: " + el.getAttribute('rotation').x + ";rotationY: " + el.getAttribute('rotation').y + ";rotationZ: " + el.getAttribute('rotation').z;
         // el.object3D.rotation.set(
         //     orientation.beta,
         //     orientation.alpha,
@@ -643,7 +643,7 @@
 
         el.object3D.quaternion.setFromEuler(new THREE.Euler(THREE.Math.degToRad(orientation.beta), THREE.Math.degToRad(orientation.alpha), -THREE.Math.degToRad(orientation.gamma), 'YXZ'));
         el.object3D.quaternion.multiply(new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));
-        document.querySelector("#test_el").innerText = "3orientation.alpha: " + THREE.Math.degToRad(orientation.alpha) + ";orientation.beta: " + THREE.Math.degToRad(orientation.beta) + ";orientation.gamma: " + THREE.Math.degToRad(orientation.gamma);
+        document.querySelector("#test_el2").innerText = "1rotationX: " + el.getAttribute('rotation').x + ";rotationY: " + el.getAttribute('rotation').y + ";rotationZ: " + el.getAttribute('rotation').z;
     });
 
 }).call(this);
