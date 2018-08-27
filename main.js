bd30e392fb5470dd7aeb3496f48016c39c3d8ac4
HiepDD <duong.dinh.hiep@usol-v.com.vn>
2018-08-27 15:01:32 +0700

    12

--- main.js
+++ main.js
@@ -434,12 +434,16 @@
 
         handlerOrientation: function (evt) {
 
-            var heading = null;
+            this.cAlpha = evt.alpha;
+            this.cBeta = evt.beta;
+            this.cGamma = evt.gamma;
+
+            //var heading = null;
 
             if (typeof (evt.webkitCompassHeading) != 'undefined') {
 
                 if (evt.webkitCompassAccuracy < 50) {
-                    heading = evt.webkitCompassHeading;
+                    //heading = evt.webkitCompassHeading;
                 } else {
                     console.warn('webkitCompassAccuracy is evt.webkitCompassAccuracy');
                 }
@@ -447,9 +451,6 @@
             } else if (evt.alpha !== null) {
                 if (evt.absolute === true || typeof (evt.absolute) == 'undefined') {
                     //heading = CompassUtils.getCompassHeading(evt.alpha, evt.beta, evt.gamma);
-                    this.cAlpha = evt.alpha;
-                    this.cBeta = evt.beta;
-                    this.cGamma = evt.gamma;
                 } else {
                     console.warn('evt.absolute === false');
                 }
