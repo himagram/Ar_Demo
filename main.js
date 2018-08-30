34054ce29de6cfb7702615c66b8ecf59acf2147e
HiepDD <duong.dinh.hiep@usol-v.com.vn>
2018-08-30 16:24:34 +0700

    zzzz

--- main.js
+++ main.js
@@ -558,9 +558,14 @@
             //if(worldAxis == null){
                 worldAxis = this.el.object3D.getWorldDirection().normalize();
             //}
-            
+
+            var newEuler = new THREE.Euler(THREE.Math.degToRad(this.cBeta), THREE.Math.degToRad(this.cAlpha), -THREE.Math.degToRad(this.cGamma), 'YXZ')
+
+            var eulerRotation = cameraRotation.rotation;
             document.querySelector("#test_el").innerText = "cameraRotation: " + cameraRotation;
-            document.querySelector("#test_el2").innerText = "heading: " + hd;
+            document.querySelector("#test_el2").innerText = "eulerX: " + eulerRotation.x + "\neulerY: " + eulerRotation.y + "\neulerZ: " + eulerRotation.z
+                + "\nNewX: " + newEuler.x + "\nNewY: " + newEuler.y + "\nNewZ: " + newEuler.z;
+
             // this.el.object3D.quaternion.setFromAxisAngle(axis, 0.01);
             // var cameraQuaternion = this.el.object3D.quaternion;
             i = i + 1;
