68cff9f85b7e9f4ff112c87d94fff79ca1f6c0cb
HiepDD <duong.dinh.hiep@usol-v.com.vn>
2018-09-05 17:44:13 +0700

    vcxvcx

--- main.js
+++ main.js
@@ -501,6 +501,10 @@
         },
 
         updateRotation: function () {
+
+            this.el.object3D.quaternion.setFromEuler(new THREE.Euler(THREE.Math.degToRad(this.cBeta), THREE.Math.degToRad(this.cAlpha), -THREE.Math.degToRad(this.cGamma), 'YXZ'));
+            this.el.object3D.quaternion.multiply(new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));  // X軸を中心に90度回転します。
+
             var heading = 360 - this.heading;
 
             var deviceOrientation = CompassUtils.getBrowserOrientation();
@@ -520,10 +524,9 @@
 
             this.lookControls.yawObject.rotation.y = THREE.Math.degToRad(offset);
             
-            this.el.object3D.quaternion.setFromEuler(new THREE.Euler(THREE.Math.degToRad(this.cBeta), THREE.Math.degToRad(this.cAlpha), -THREE.Math.degToRad(this.cGamma), 'YXZ'));
-            this.el.object3D.quaternion.multiply(new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));  // X軸を中心に90度回転します。
+            
 
-            document.querySelector("#test_el").innerText = "quater";
+            document.querySelector("#test_el").innerText = "112312";
         },
 
         remove: function () {
