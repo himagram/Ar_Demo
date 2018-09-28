cac3ba95224987312511df148530cddf417d9f1b
HiepDD <duong.dinh.hiep@usol-v.com.vn>
2018-09-28 11:21:42 +0700

    123

--- main.js
+++ main.js
@@ -322,9 +322,10 @@
 			if(event.webkitCompassHeading) {
 				// Apple works only with this, alpha doesn't work
 				compassdir = event.webkitCompassHeading;  
+				evt.alpha = 360 - compassdir;
 			}
 			
-			document.querySelector("#test_el2").innerText = "compassdir: " + compassdir + "\nAlpha: " + evt.alpha;
+			document.querySelector("#test_el2").innerText = "88compassdir: " + compassdir + "\nAlpha: " + evt.alpha;
 		
             this.el.object3D.quaternion.setFromEuler(new THREE.Euler(THREE.Math.degToRad(evt.beta), THREE.Math.degToRad(evt.alpha), -THREE.Math.degToRad(evt.gamma), 'YXZ'));
             this.el.object3D.quaternion.multiply(new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));  // X軸を中心に90度回転します。
